b08167aed93909132b0db2b9de87fa4e22f55e64
Linh Nguyen <tuleism@gmail.com>
2021-10-15 23:25:41 +0800

    tracing for client

--- zio/src/main/scala/com/github/tuleism/GrpcTracing.scala
+++ zio/src/main/scala/com/github/tuleism/GrpcTracing.scala
@@ -3,9 +3,9 @@ package com.github.tuleism
 import io.grpc.{Metadata, Status}
 import io.opentelemetry.api.trace.{SpanKind, StatusCode}
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator
-import io.opentelemetry.context.propagation.{TextMapGetter, TextMapPropagator}
+import io.opentelemetry.context.propagation.{TextMapGetter, TextMapPropagator, TextMapSetter}
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes
-import scalapb.zio_grpc.{RequestContext, ZTransform}
+import scalapb.zio_grpc.{RequestContext, ZClientInterceptor, ZTransform}
 import zio.stream.ZStream
 import zio.{Has, ZIO}
 import zio.telemetry.opentelemetry.Tracing
@@ -24,6 +24,14 @@ object GrpcTracing {
       )
   }
 
+  private val metadataSetter: TextMapSetter[Metadata] = (carrier, key, value) =>
+    carrier.put(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER), value)
+
+  val contextPropagationClientInterceptor: ZClientInterceptor[Tracing] = ZClientInterceptor.headersUpdater {
+    (_, _, metadata) =>
+      metadata.wrapM(Tracing.inject(propagator, _, metadataSetter))
+  }
+
   private def withSemanticAttributes[R, A](effect: ZIO[R, Status, A]): ZIO[Tracing with R, Status, A] =
     Tracing.setAttribute(SemanticAttributes.RPC_SYSTEM.getKey, "grpc") *>
       effect
@@ -36,6 +44,14 @@ object GrpcTracing {
             ),
           _ => Tracing.setAttribute(SemanticAttributes.RPC_GRPC_STATUS_CODE.getKey, Status.OK.getCode.value())
         )
+
+  def clientTracingTransform[R]: ZTransform[R, Status, R with Tracing] =
+    new ZTransform[R, Status, R with Tracing] {
+
+      def effect[A](io: ZIO[R, Status, A]): ZIO[R with Tracing, Status, A] = withSemanticAttributes(io)
+
+      def stream[A](io: ZStream[R, Status, A]): ZStream[R with Tracing, Status, A] = ???
+    }
 
   def serverTracingTransform[R]: ZTransform[R, Status, R with Tracing with Has[RequestContext]] =
     new ZTransform[R, Status, R with Tracing with Has[RequestContext]] {
--- zio/src/main/scala/com/github/tuleism/ZClient.scala
+++ zio/src/main/scala/com/github/tuleism/ZClient.scala
@@ -2,35 +2,63 @@ package com.github.tuleism
 
 import io.grpc.ManagedChannelBuilder
 import io.grpc.examples.helloworld.helloworld.ZioHelloworld.GreeterClient
-import io.grpc.examples.helloworld.helloworld.HelloRequest
+import io.grpc.examples.helloworld.helloworld.{GreeterGrpc, HelloRequest}
+import io.opentelemetry.api.trace.{SpanKind, StatusCode}
 import scalapb.zio_grpc.ZManagedChannel
-import zio.console._
 import zio._
+import zio.config.syntax._
+import zio.console._
+import zio.magic._
+import zio.telemetry.opentelemetry.Tracing
+import zio.telemetry.opentelemetry.TracingSyntax._
 
 object ZClient extends zio.App {
   private val clientLayer = GreeterClient.live(
     ZManagedChannel(
-      ManagedChannelBuilder.forAddress("localhost", 9000).usePlaintext()
+      ManagedChannelBuilder.forAddress("localhost", 9000).usePlaintext(),
+      Seq(GrpcTracing.contextPropagationClientInterceptor)
     )
   )
 
-  private val singleHello = GreeterClient.sayHello(HelloRequest("World"))
+  private def errorToStatusCode[E]: PartialFunction[E, StatusCode] = { case _ => StatusCode.ERROR }
 
-  private val multipleHellos = ZIO.collectAllParN(5)(
-    List(
-      GreeterClient.sayHello(HelloRequest("1", Some(1))),
-      GreeterClient.sayHello(HelloRequest("2", Some(2))),
-      GreeterClient.sayHello(HelloRequest("3", Some(3))),
-      GreeterClient.sayHello(HelloRequest("4", Some(4))),
-      GreeterClient.sayHello(HelloRequest("5", Some(5)))
+  private def sayHello(request: HelloRequest) =
+    GreeterClient
+      .sayHello(request)
+      .span(
+        GreeterGrpc.METHOD_SAY_HELLO.getFullMethodName,
+        SpanKind.CLIENT,
+        errorToStatusCode
+      )
+
+  private val singleHello = sayHello(HelloRequest("World")).span("singleHello", toErrorStatus = errorToStatusCode)
+
+  private val multipleHellos = ZIO
+    .collectAllParN(5)(
+      List(
+        sayHello(HelloRequest("1", Some(1))),
+        sayHello(HelloRequest("2", Some(2))),
+        sayHello(HelloRequest("3", Some(3))),
+        sayHello(HelloRequest("4", Some(4))),
+        sayHello(HelloRequest("5", Some(5)))
+      )
     )
-  )
+    .span("multipleHellos", toErrorStatus = errorToStatusCode)
 
-  private val invalidHello = GreeterClient.sayHello(HelloRequest("Invalid", Some(-1))).ignore
+  private val invalidHello =
+    sayHello(HelloRequest("Invalid", Some(-1))).ignore.span("invalidHello", toErrorStatus = errorToStatusCode)
 
   private def myAppLogic =
     singleHello *> multipleHellos *> invalidHello *> putStrLn("Done")
 
+  private val requirements = ZLayer
+    .wire[ZEnv with Tracing](
+      ZEnv.live,
+      AppConfig.live.narrow(_.tracing),
+      ZTracer.live("hello-client"),
+      Tracing.live
+    ) >+> clientLayer
+
   def run(args: List[String]): URIO[ZEnv, ExitCode] =
-    myAppLogic.provideCustomLayer(clientLayer).exitCode
+    myAppLogic.provideCustomLayer(requirements).exitCode
 }
